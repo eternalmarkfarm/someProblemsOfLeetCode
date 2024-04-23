0f704f7a6c5eb2f53a9d0b5d29982d9866fb7c54
eternalmarkfarm <nksepte@gmail.com>
2024-04-23 18:03:33 +0500

    Longest Common Prefix

---
+++ LongestCommonPrefix.cpp
@@ -0,0 +1,39 @@
+#include<vector>
+#include<string>
+#include<algorithm>
+
+
+
+bool compareByLength(const std::string& a, const std::string& b) {
+    return a.size() < b.size();
+}
+
+
+std::string longestCommonPrefix(std::vector<std::string>& strs) {
+
+    std::sort(strs.begin(), strs.end(), compareByLength);
+
+
+    std::string lcp = "";
+    int sizeS = strs[0].size();
+    for (size_t i = 0; i < sizeS; ++i) {
+        int count = 1;
+        int next = 1;
+        int y = 1;
+
+        while (y < strs.size()) {
+            if (strs[0][i] == strs[next][i]) {
+                ++count;
+                ++next;
+                ++y;
+            }
+            else {
+                return lcp;
+            }
+        }
+        if (count == strs.size()) {
+            lcp += std::string(1, strs[0][i]);
+        }
+    }
+    return lcp;
+}
