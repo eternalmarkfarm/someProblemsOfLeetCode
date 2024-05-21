5b99cd87a3d25ab0028f177cd2971bef0735fc7f
eternalmarkfarm <nksepte@gmail.com>
2024-05-21 19:53:11 +0500

    Maximum Depth of Binary Tree

---
+++ MaximumDepthofBinaryTree.cpp
@@ -0,0 +1,41 @@
+#include <queue>
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    std::queue<TreeNode*> q;
+    q.push(root);
+    int count = 0;
+
+    while (!q.empty()) {
+        int levelSize = q.size();
+
+        for (int i = 0; i < levelSize; ++i) {
+            TreeNode* node = q.front();
+            q.pop();
+            std::cout << node->val << " ";
+
+            if (node->left != nullptr) {
+                q.push(node->left);
+            }
+            if (node->right != nullptr) {
+                q.push(node->right);
+            }
+        }
+        ++count;
+        std::cout << std::endl;
+    }
+    std::cout << count;
+}
