01abf5c2960b0e5ecd99a9e33c6111d8d8ac2e97
eternalmarkfarm <nksepte@gmail.com>
2024-05-22 23:01:13 +0500

    Conver Sorted Array to Binary Tree

---
+++ CSABST.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <vector>
+
+
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+
+TreeNode* sortedArrayToBST(std::vector<int>& nums) {
+    return sortedArrayToBST(nums, 0, nums.size() - 1);
+}
+
+TreeNode* sortedArrayToBST(std::vector<int>& nums, int start, int end) {
+    if (start > end) {
+        return nullptr;
+    }
+    int mid = start + (end - start) / 2;
+    TreeNode* node = new TreeNode(nums[mid]);
+    node->left = sortedArrayToBST(nums, start, mid - 1);
+    node->right = sortedArrayToBST(nums, mid + 1, end);
+    return node;
+}
+
+
+
+
