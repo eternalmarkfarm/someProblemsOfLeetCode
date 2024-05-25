5e4aefe709f2b6576c6cc818e3ab807ddcf35477
eternalmarkfarm <nksepte@gmail.com>
2024-05-25 14:53:07 +0500

    Balanced Binary Tree

---
+++ BalanceddBinaryTree.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+int height(TreeNode* root) {
+    if (root == nullptr) {
+        return 0;
+    }
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    
+    if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
+        return -1;
+    }
+    
+    return std::max(leftHeight, rightHeight) + 1;
+}
+
+bool isBalanced(TreeNode* root) {
+    return height(root) != -1;
+}
